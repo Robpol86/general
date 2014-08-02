f774e528edd8a22db7f1f6c1ae677c627165470c
Robpol86 <robpol86@gmail.com>
2014-08-02 13:54:21 -0700

    Fixed git prompt.

--- bash_profile.sh
+++ bash_profile.sh
@@ -9,13 +9,11 @@ export LC_CTYPE=en_US.UTF-8
 export CLICOLOR=1
 export LSCOLORS=gxGxFxdxbxDxDxBxBxExEx
 
-if [ -e "/opt/git/etc/bash_completion.d/git-completion.bash" ]; then
-  if [ -e "/opt/git/etc/bash_completion.d/git-prompt.sh" ]; then
-    # Both of these files are required.
-    source "/opt/git/etc/bash_completion.d/git-completion.bash"
-    source "/opt/git/etc/bash_completion.d/git-prompt.sh"
-    export PS1='[\h \[\033[0;36m\]\W\[\033[0m\]$(__git_ps1 " \[\033[1;32m\](%s)\[\033[0m\]")]\$ '
-  fi
+alias grep='grep --color=auto' 2>/dev/null
+
+if [ -f "/usr/local/etc/bash_completion" ]; then
+  source "/usr/local/etc/bash_completion"
+  export PS1='[\h \[\033[0;36m\]\W\[\033[0m\]$(__git_ps1 " \[\033[1;32m\](%s)\[\033[0m\]")]\$ '
 else
   export PS1='[\h \[\033[0;36m\]\W\[\033[0m\]]\$ '
 fi
